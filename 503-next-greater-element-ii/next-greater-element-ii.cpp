6ef6770a8816263c8121ad742492275a713d19f0
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-02 15:27:03 +0530

    Time: 4 ms (58.92%) | Memory: 28.5 MB (64.79%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nge(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= arr[i])
+                st.pop();
+            st.push(arr[i]);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= arr[i])
+                st.pop();
+
+            if (st.empty())
+                nge[i] = -1;
+            else
+                nge[i] = st.top();
+
+            st.push(arr[i]);
+        }
+        return nge;
+    }
+};
