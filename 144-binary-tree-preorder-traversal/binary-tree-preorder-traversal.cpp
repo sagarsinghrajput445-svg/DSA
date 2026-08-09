19f36c01aa725b456ace2ac4981d9e9ed2caa95c
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-09 15:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (74.76%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,19 +11,15 @@
  */
 class Solution {
 public:
-
+    void helper(TreeNode* root , vector<int>& ans){
+        if(root==NULL) return;
+        ans.push_back(root->val);
+        helper(root->left,ans);
+        helper(root->right,ans);
+    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>v;
-        if(root==NULL) return v;
-        v.push_back(root->val);
-        vector<int> left=preorderTraversal(root->left);
-        for(int i=0;i<left.size();i++){
-            v.push_back(left[i]);
-        }
-        vector<int> right=preorderTraversal(root->right);
-        for(int i=0;i<right.size();i++){
-            v.push_back(right[i]);
-        }
-        return v;
+        vector<int>ans;
+        helper(root,ans);
+        return ans;
     }
 };
