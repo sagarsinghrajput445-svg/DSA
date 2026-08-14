47a1189c964c4c5bd697089c9c9ccac515bcbcbe
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-14 21:56:26 +0530

    Time: 37 ms (8.62%) | Memory: 103 MB (57.48%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -3,22 +3,18 @@ public:
     vector<int> dailyTemperatures(vector<int>& arr) {
         int n = arr.size();
         vector<int> nge(n);
-        stack<int> st;
-        stack<int> pt;
+        stack<pair<int,int>> st;
         nge[n - 1] = 0;
-        st.push(arr[n - 1]);
-        pt.push(n-1);
+        st.push({arr[n - 1],n-1});
         for (int i = n - 2; i >= 0; i--) {
-            while (st.size() > 0 && st.top() <= arr[i]) {
+            while (st.size() > 0 && st.top().first <= arr[i]) {
                 st.pop();
-                pt.pop();
             }
             if (st.size() == 0)
                 nge[i] = 0;
             else
-                nge[i] = pt.top()-i;
-            st.push(arr[i]);
-            pt.push(i);
+                nge[i] = st.top().second-i;
+            st.push({arr[i],i});
         }
         return nge;
     }
