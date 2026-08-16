7ac6805d49df4c496c5e151a04e6649227c32d67
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-16 11:39:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (66.37%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,10 +1,36 @@
 class Solution {
 public:
+    int find(vector<int>& nums, int lo, int hi) {
+        while (lo < hi) {
+            while (lo < hi && nums[lo] == nums[lo + 1])
+                lo++;
+            while (lo < hi && nums[hi] == nums[hi - 1])
+                hi--;
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[hi])
+                lo = mid + 1;
+            else if (nums[mid] < nums[hi])
+                hi = mid;
+            else
+                hi--;
+        }
+        return lo;
+    }
+    void binarySearch(vector<int>& nums, int lo, int hi, int& min){
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]<min){
+                min=nums[mid];
+            }
+            else hi=mid-1;
+        }
+    }
     int findMin(vector<int>& nums) {
-        int mn=INT_MAX;
-       for(int i=0;i<nums.size();i++){
-            if(nums[i]<mn) mn=nums[i];
-       }
-       return mn;
+        int n=nums.size();
+        int pivot=find(nums,0,n-1);
+        int min=INT_MAX;
+        binarySearch(nums,0,pivot-1,min);
+        binarySearch(nums,pivot,n-1,min);
+        return min;
     }
 };
