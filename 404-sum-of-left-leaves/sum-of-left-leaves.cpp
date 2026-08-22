feefbc0c4e467da64159a82473772279f6e1b513
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-22 12:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (58.54%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void helper(TreeNode* root, int& sum){
+        if(root==NULL) return;
+        if(root->left){
+            if(root->left->left==NULL && root->left->right==NULL){
+                sum+=root->left->val;
+            }
+        }
+        helper(root->left,sum);
+        helper(root->right,sum);
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum=0;
+        helper(root,sum);
+        return sum;
+    }
+};
