a372629230738e35eec8f3fb2eb5ba93f18bee4b
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-30 22:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (94.86%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,28 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in,
-                    int inlo, int inhi) {
-        if (prelo > prehi)
-            return NULL;
-        TreeNode* root = new TreeNode(pre[prelo]);
-        if (prelo == prehi)
-            return root;
-        int i = inlo;
-        while (i <= inhi) {
-            if (in[i] == pre[prelo])
-                break;
-            i++;
+    void insert(TreeNode* root, int val){
+        if(root==NULL) root=new TreeNode(val);
+        else if(root->val > val){ // go left
+            if(root->left==NULL){ // attach it here
+                root->left=new TreeNode(val);
+            }
+            else insert(root->left,val);
         }
-        int leftCount = i - inlo;
-        int rightCount = inhi - i;
-        root->left = build(pre, prelo + 1, prelo + leftCount, in, inlo, i - 1);
-        root->right = build(pre, prelo + leftCount + 1, prehi, in, i + 1, inhi);
-        return root;
+        else{// root->val<val : go left
+            if(root->right==NULL){ // attach is here
+                root->right=new TreeNode(val);
+            }
+            else insert(root->right,val);
+        }
     }
     TreeNode* bstFromPreorder(vector<int>& pre) {
-        vector<int> in = pre; // copy ban gayi
-        sort(in.begin(), in.end());
-        int n = pre.size();
-        return build(pre, 0, n - 1, in, 0, n - 1);
+        TreeNode* root=new TreeNode(pre[0]);
+        for(int i=1;i<pre.size();i++){
+           insert(root,pre[i]);
+        }
+        return root;
     }
 };
