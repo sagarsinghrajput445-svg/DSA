d6f77fff36d4589de2fa47a458fd0dff7a69011b
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-08-30 21:49:23 +0530

    Time: 1 ms (25.91%) | Memory: 16.7 MB (9.47%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    TreeNode* build(vector<int>& pre, int prelo, int prehi, vector<int>& in,
+                    int inlo, int inhi) {
+        if (prelo > prehi)
+            return NULL;
+        TreeNode* root = new TreeNode(pre[prelo]);
+        if (prelo == prehi)
+            return root;
+        int i = inlo;
+        while (i <= inhi) {
+            if (in[i] == pre[prelo])
+                break;
+            i++;
+        }
+        int leftCount = i - inlo;
+        int rightCount = inhi - i;
+        root->left = build(pre, prelo + 1, prelo + leftCount, in, inlo, i - 1);
+        root->right = build(pre, prelo + leftCount + 1, prehi, in, i + 1, inhi);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+        vector<int> in = pre; // copy ban gayi
+        sort(in.begin(), in.end());
+        int n = pre.size();
+        return build(pre, 0, n - 1, in, 0, n - 1);
+    }
+};
