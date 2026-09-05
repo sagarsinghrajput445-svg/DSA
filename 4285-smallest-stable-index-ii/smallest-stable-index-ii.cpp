81431ffe1c9592763d6a2fb632ea9f1f7138ba6a
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-09-05 22:03:30 +0530

    Time: 12 ms (51.64%) | Memory: 210.8 MB (56.49%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>prefix(nums.size());
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefix[i]=max(prefix[i-1],nums[i]);
+        }
+        vector<int>suffix(nums.size());
+        suffix[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=min(suffix[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(prefix[i]-suffix[i]<=k) return i;
+        }
+        return -1;
+    }
+};
