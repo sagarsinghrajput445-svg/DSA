ebaf9934118f20adc9936487f9ec95a04ad7c246
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-09-06 11:23:17 +0530

    Time: 1 ms (24.16%) | Memory: 12.1 MB (46.22%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            m[arr[i]]++;
+        }
+        unordered_set<int>s;
+        for(auto p: m){
+            s.insert(p.second);
+        }
+        if(s.size()==m.size()) return true;
+        else return false;
+    }
+};
