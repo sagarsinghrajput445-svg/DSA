a674a9380677814f4a73609322374eabde430a6e
sagarsinghrajput445-svg <sagarsinghrajput445@gmail.com>
2026-09-15 11:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (12.44%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    typedef pair<int,int>pi;
+    typedef pair<int,int> pi;
+    struct compare {
+        bool operator()(pi a, pi b) {
+            if(a.first == b.first)
+                return a.second < b.second;  // bada element pehle
+            return a.first > b.first;        // chhoti frequency pehle
+        }
+    };
     vector<int> frequencySort(vector<int>& nums) {
-         unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++) {
             mp[nums[i]]++;
         }
-        priority_queue<pi,vector<pi>,greater<pi>>pq;
-        for(auto x: mp){
-            pq.push({x.second,-x.first});
+        // custom comparator -> compare
+        priority_queue<pi, vector<pi>, compare> pq;
+        for(auto x : mp) {
+            pq.push({x.second, x.first});
         }
-        vector<int>ans;
-        while(pq.size()>0){
-            int ele=-pq.top().second;
-            int fre=pq.top().first;
-            while(fre>0){
+        vector<int> ans;
+        while(!pq.empty()) {
+            int ele = pq.top().second;
+            int fre = pq.top().first;
+            while(fre > 0) {
                 ans.push_back(ele);
                 fre--;
             }
